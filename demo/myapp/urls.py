7d23416189ae0329a4d203442a38b6b0b9251f75
RB <82284311+Roman123-game@users.noreply.github.com>
2025-02-04 16:57:47 +0200

    workind todo list

--- demo/myapp/urls.py
+++ demo/myapp/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('todos/', views.todos, name='todos'),
 ]
--- demo/myapp/views.py
+++ demo/myapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TodoItem
 
 def home(request):
     return render(request,"home.html")
+
+
+def todos(request):
+    items= TodoItem.objects.all()
+    return render(request,"todos.html",{"todos":items})
